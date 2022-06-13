81fd8b6338951417145859814153b23af20c15cf
k2sebeom <moses97@gmail.com>
2022-06-13 18:35:35 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import { useState, forwardRef } from 'react';
+import { Tooltip, IconButton } from '@mui/material';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
+
 function App() {
+  const [value, setValue] = useState(50)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100vh'
+    }}>
+      <div
+        style={{
+          width: '100%',
+          height: 50,
+          backgroundColor: 'skyblue',
+          position: 'absolute',
+          top: 0
+        }}
+      >
+
+      </div>
+      <div>
+        <Tooltip title="Hello" arrow>
+          <IconButton onClick={() => {
+            window.alert('Dont touch me!');
+          }}>
+            <AccessAlarmIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
     </div>
   );
 }
